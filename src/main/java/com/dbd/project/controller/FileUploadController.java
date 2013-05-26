3f8dd95043687df5d81320a98073047a756ceceb
anda <anda_demian2005@yahoo.com>
2013-05-26 12:17:02 +0300

    Added FileUpload functionality using GridFs

--- src/main/java/com/dbd/project/controller/FileUploadController.java
+++ src/main/java/com/dbd/project/controller/FileUploadController.java
@@ -1,35 +1,58 @@
 package com.dbd.project.controller;
 
 import java.io.IOException;
-import java.text.ParseException;
-import java.util.Iterator;
-import java.util.LinkedList;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.FileCopyUtils;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.validation.BindException;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.SimpleFormController;
 
+import com.dbd.project.dao.FileUploadDao;
 import com.dbd.project.model.FileUpload;
+import com.dbd.project.validator.FileUploadValidator;
 
 /**
  * Handles the requests for application import feature.
  * 
  * @author anda
  */
-@Controller
-public class FileUploadController {
-	
+public class FileUploadController extends SimpleFormController {
+	@Autowired
+	private FileUploadDao fileStorageDAOMongoImpl;
+	@Autowired
+	private FileUploadValidator validator;
+
+	public FileUploadController() {
+		setCommandClass(FileUpload.class);
+		setCommandName("customerForm");
+	}
+
+	public ModelAndView onSubmit(HttpServletRequest request,
+			HttpServletResponse response, Object command, BindException errors)
+			throws Exception {
+
+		// cast the bean
+		FileUpload bean = (FileUpload) command;
+
+		// let's see if there's content there
+		MultipartFile file = bean.getFile();
+		validator.validate(bean, errors);
+		// if we don;t have validation errors
+		if (!errors.hasErrors()) {
+			if (file == null) {
+				return null;
+			}
+			fileStorageDAOMongoImpl.save(file.getInputStream(),
+					file.getContentType(), file.getOriginalFilename());
+		}
+		// well, let's do nothing with the bean for now and return
+		return super.onSubmit(request, response, command, errors);
+	}
+
 }
--- src/main/java/com/dbd/project/dao/FileUploadDao.java
+++ src/main/java/com/dbd/project/dao/FileUploadDao.java
@@ -2,7 +2,12 @@ package com.dbd.project.dao;
 
 import java.io.InputStream;
 
+import com.dbd.project.model.FileUpload;
+import com.mongodb.gridfs.GridFSDBFile;
+
 public interface FileUploadDao {
 
 	public String save(InputStream inputStream, String contentType, String filename);
+	public void deleteFile(FileUpload file);
+	public GridFSDBFile find(String name);
 }
